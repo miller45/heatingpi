c7d74e6b7183fb246318720b7dfbb937c9c04500
RKlein <rklein@roseninspection.net>
2022-12-14 16:42:08 +0100

    valve position testing

--- test_valve.py
+++ test_valve.py
@@ -5,6 +5,8 @@ from unittest.mock import patch
 import math
 import time
 
+FULLTURNTIME = 120
+
 
 class TestValveClass(unittest.TestCase):
 
@@ -47,3 +49,63 @@ class TestValveClass(unittest.TestCase):
         self.assertEqual(True, v.Relay_State1)
         self.assertEqual(True, v.Relay_State2)
         self.assertEqual("INVALID", v.direction)
+
+    def test_position_hotterfullturn(self):
+        currtime = math.trunc(time.time() * 1000)
+        nexttime = currtime + FULLTURNTIME * 1000
+        v = valve.Valve()
+        v.current_position = 0
+        v.update(currtime)
+        v.switchRelay2On()
+        v.update(nexttime)
+        self.assertEqual(90, v.current_position)
+
+    def test_position_hotterupperbound(self):
+        currtime = math.trunc(time.time() * 1000)
+        nexttime = currtime + (FULLTURNTIME + 30) * 1000
+        v = valve.Valve()
+        v.current_position = 0
+        v.update(currtime)
+        v.switchRelay2On()
+        v.update(nexttime)
+        self.assertEqual(90, v.current_position)
+
+    def test_position_hottertensecs(self):
+        currtime = math.trunc(time.time() * 1000)
+        nexttime = currtime + 10 * 1000
+        v = valve.Valve()
+        v.current_position = 0
+        v.update(currtime)
+        v.switchRelay2On()
+        v.update(nexttime)
+        self.assertEqual(7.5, v.current_position)
+
+    def test_position_colderfullturn(self):
+        currtime = math.trunc(time.time() * 1000)
+        nexttime = currtime + FULLTURNTIME * 1000
+        v = valve.Valve()
+        v.current_position = 90
+        v.update(currtime)
+        v.switchRelay1On()
+        v.update(nexttime)
+        self.assertEqual(0, v.current_position)
+
+    def test_position_colderlowerbound(self):
+        currtime = math.trunc(time.time() * 1000)
+        nexttime = currtime + (FULLTURNTIME + 30) * 1000
+        v = valve.Valve()
+        v.current_position = 90
+        v.update(currtime)
+        v.switchRelay1On()
+        v.update(nexttime)
+        self.assertEqual(0, v.current_position)
+
+    def test_position_coldertensecs(self):
+        currtime = math.trunc(time.time() * 1000)
+        nexttime = currtime + 20 * 1000
+        v = valve.Valve()
+        v.current_position = 90
+        v.update(currtime)
+        v.switchRelay1On()
+        v.update(nexttime)
+        self.assertEqual(75, v.current_position)
